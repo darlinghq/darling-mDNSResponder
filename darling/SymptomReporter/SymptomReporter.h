c017030f8d1daf0b6348c0d1d599e1d57a29e32a
Ariel Abreu <facekapow@outlook.com>
2020-11-01 23:40:40 -0500

    mDNSResponder binary now compiles (but doesn't link)

---
+++ darling/SymptomReporter/SymptomReporter.h
@@ -0,0 +1,10 @@
+#ifndef SYMPTOMREPORTER_H
+#define SYMPTOMREPORTER_H
+
+// not sure what these actually are
+typedef void* symptom_framework_t;
+typedef void* symptom_t;
+
+typedef int symptom_ident_t;
+
+#endif // SYMPTOMREPORTER_H
